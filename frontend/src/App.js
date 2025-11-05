4fb386e77434ff9db1140f8dff002d789c262d9c
Jorge Sanchez <jrgels99@gmail.com>
2025-11-05 17:16:06 -0600

    Se añade Tailwind al proyecto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reaca 
+          <p className="text-red-500">Learn Reaca</p> 
         </a>
       </header>
     </div>
